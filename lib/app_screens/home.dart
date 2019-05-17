53498decbdeef891549cde1aee97ab216a55f038
JasKay <oljas1@morgan.edu>
2019-05-17 01:02:02 -0400

    Put list of 2 airlines now

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -1,52 +1,75 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Center(
+       return Center(
         child: Container(
+          padding: EdgeInsets.only(left:10.0, top:40.0),
           alignment: Alignment.center,
           color: Colors.deepPurple,
-          child: Row(
-            children: <Widget>[
+          child: Column(
+          children: <Widget>[
 
-              //widget 1
-                Text(
-                "Spice Jet",
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                    decoration: TextDecoration.none,
-                    fontSize: 35.0,
-                    fontFamily: 'Raleway',
-                    fontWeight: FontWeight.w300,
-                    color: Colors.amber
-                ),  //textstyle
-              ),   //text,
+          Row(
+          children: <Widget>[
+          Expanded(child: Text(
+          //widget 1
+          "Spirit Airline",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+                decoration: TextDecoration.none,
+                fontSize: 35.0,
+                fontFamily: 'Raleway',
+                fontWeight: FontWeight.w700,
+                color: Colors.amber), //textstyle
+          )), //text, expanded
+
+      Expanded(child: Text(
+        //widget 2
+        "From BWI to Durham",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+            decoration: TextDecoration.none,
+            fontSize: 20.0,
+            fontFamily: 'Raleway',
+            fontWeight: FontWeight.w700,
+            color: Colors.pink), //textstyle
+      )), //text, expanded
+      ], //widget<>[]
+    ), //row
 
 
-              //widget 2
-               Text(
-                "From BWI to Durham",
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                    decoration: TextDecoration.none,
-                    fontSize: 75.0,
-                    fontFamily: 'Raleway',
-                    fontWeight: FontWeight.w300,
-                    color: Colors.amber
-                ),  //textstyle
-              ),   //text
-            ],
-          )
-          //width: 240,
-          //height: 120,
-         // margin: EdgeInsets.only(left:15),   //for margin
 
-        ) //container
-    ); //center
+            Row(
+              children: <Widget>[
+                Expanded(child: Text(
+                  //widget 1
+                  "United Airlines",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 35.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w700,
+                      color: Colors.amber), //textstyle
+                )), //text, expanded
+
+                Expanded(child: Text(
+                  //widget 2
+                  "From JFK to LAX",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 20.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w700,
+                      color: Colors.pink), //textstyle
+                )), //text, expanded
+              ], //widget<>[]
+            ), //row
+
+        ], //<widget>[]
+    ))); //column container, CrossAxisAlignment.center
   }
 }
-
-
-
