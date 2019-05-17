18ce568571f0e7d3a0e563c10fcc92679b109044
JasKay <oljas1@morgan.edu>
2019-05-17 00:00:38 -0400

    Current change

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -8,20 +8,41 @@ class Home extends StatelessWidget {
         child: Container(
           alignment: Alignment.center,
           color: Colors.deepPurple,
+          child: Row(
+            children: <Widget>[
+
+              //widget 1
+                Text(
+                "Spice Jet",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 35.0,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w300,
+                    color: Colors.amber
+                ),  //textstyle
+              ),   //text,
+
+
+              //widget 2
+               Text(
+                "From BWI to Durham",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 75.0,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w300,
+                    color: Colors.amber
+                ),  //textstyle
+              ),   //text
+            ],
+          )
           //width: 240,
           //height: 120,
          // margin: EdgeInsets.only(left:15),   //for margin
-          child: Text(
-              "Flight",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(
-                decoration: TextDecoration.none,
-                fontSize: 75.0,
-                fontFamily: 'Raleway',
-                fontWeight: FontWeight.w300,
-                color: Colors.amber
-              ),  //textstyle
-          ),   //text
+
         ) //container
     ); //center
   }
